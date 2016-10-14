6b55945fa5c486cd3a4264830e910f981fc5bee2
osbaldo-esquivel <osbaldo.esquivel@gmail.com>
2016-10-13 22:03:39 -0700

    Updates

--- concepts/room.py
+++ concepts/room.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import item
+#import item
 
 '''
 Very rough draft of room object. Getting
@@ -8,8 +8,42 @@ thoughts down.
 '''
 
 class Room(object):
-  def __init__(self,desc,conn, invItem, intItem):
-      self.desc = desc
-      self.conn = conn
-      self.invItem = invItem
-      self.intItem = intItem
+  def __init__(self,name,longDesc,shortDesc,conn1,conn2,invItem,intItem):
+	self.name = name
+	self.longDesc = longDesc
+	self.shortDesc = shortDesc
+	self.conn1 = conn1
+	self.conn2 = conn2
+	self.invItem = invItem
+	self.intItem = intItem
+	
+def readFile(room):
+	with open("limboFile.txt") as f:
+		f = f.read().splitlines()
+		
+	seg = [x for x in f if x and not x.strip().startswith('!')]
+	
+	room.name = seg[0]
+	room.longDesc = seg[1]
+	room.shortDesc = seg[2]
+	room.conn1 = seg[3]
+	room.conn2 = seg[4]
+	room.invItem = seg[5]
+	room.intItem = seg[6]
+	
+def main():
+	limboRoom = Room(" "," "," ",0,0," "," ")
+	
+	readFile(limboRoom)
+	
+	print limboRoom.name
+	print limboRoom.longDesc
+	print limboRoom.shortDesc
+	print limboRoom.conn1
+	print limboRoom.conn2
+	print limboRoom.invItem
+	print limboRoom.intItem
+	
+if __name__=="__main__": main()
+		
+	
