7084543332b79a68d17944667a312d99651bd1d0
osbaldo-esquivel <osbaldo.esquivel@gmail.com>
2016-10-23 16:51:08 -0700

    Add room files

--- concepts/room.py
+++ concepts/room.py
@@ -8,17 +8,17 @@ thoughts down.
 '''
 
 class Room(object):
-  def __init__(self,name,longDesc,shortDesc,conn1,conn2,invItem,intItem):
-	self.name = name
-	self.longDesc = longDesc
-	self.shortDesc = shortDesc
-	self.conn1 = conn1
-	self.conn2 = conn2
-	self.invItem = invItem
-	self.intItem = intItem
+	def __init__(self,name,longDesc,shortDesc,conn1,conn2,invItem,intItem):
+		self.name = name
+		self.longDesc = longDesc
+		self.shortDesc = shortDesc
+		self.conn1 = conn1
+		self.conn2 = conn2
+		self.invItem = invItem
+		self.intItem = intItem
 	
-def readFile(room):
-	with open("limboFile.txt") as f:
+def readFile(room,fName):
+	with open(fName) as f:
 		f = f.read().splitlines()
 		
 	seg = [x for x in f if x and not x.strip().startswith('!')]
@@ -31,18 +31,26 @@ def readFile(room):
 	room.invItem = seg[5]
 	room.intItem = seg[6]
 	
+	
 def main():
-	limboRoom = Room(" "," "," ",0,0," "," ")
+	roomArr = []
 	
-	readFile(limboRoom)
+	roomArr.append(limboRoom = Room(" "," "," ",0,0," ", " "))
+	roomArr.append(lustRoom = Room(" "," "," ",0,0," ", " "))
+	roomArr.append(glutRoom = Room(" "," "," ",0,0," ", " "))
+	roomArr.append(greedRoom = Room(" "," "," ",0,0," ", " "))
+	roomArr.append(wrathRoom = Room(" "," "," ",0,0," ", " "))
+	roomArr.append(heresyRoom = Room(" "," "," ",0,0," ", " "))
+	roomArr.append(viRoom = Room(" "," "," ",0,0," ", " "))
+	roomArr.append(fraudRoom = Room(" "," "," ",0,0," ", " "))
+	roomArr.append(treachRoom = Room(" "," "," ",0,0," ", " "))
+	roomArr.append(centerRoom = Room(" "," "," ",0,0," ", " "))
 	
-	print limboRoom.name
-	print limboRoom.longDesc
-	print limboRoom.shortDesc
-	print limboRoom.conn1
-	print limboRoom.conn2
-	print limboRoom.invItem
-	print limboRoom.intItem
+	fileArr = ["limboFile.txt","lustFile.txt","glutFile.txt","greedFile.txt","wrathFile.txt","heresyFile.txt",
+				   "viFile.txt","fraudFile.txt","treachFile.txt","centerFile.txt"]
+	
+	for x in roomArr:
+		readFile(roomArr[x],fileArr[x])
 	
 if __name__=="__main__": main()
 		
