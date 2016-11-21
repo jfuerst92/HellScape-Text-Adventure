9341353521c4738ab1a48a138fb05eb7313a04a4
Christopher King <kingc4@flip2.engr.oregonstate.edu>
2016-11-21 11:23:20 -0800

    Added exception handling to thesaurus.py

--- Game/thesaurus.py
+++ Game/thesaurus.py
@@ -10,8 +10,17 @@ def apiRequest(word):
 	response = urllib2.urlopen("http://words.bighugelabs.com/api/2/" + apiKey + "/" + word + "/json").read()
 	return json.loads(response)
 
+def apiRequest2(word):
+	try: response = urllib2.urlopen("http://words.bighugelabs.com/api/2/" + apiKey + "/" + word + "/json")
+	except urllib2.URLError as e:
+		return None
+	return json.loads(response.read())
+
 def getSynonymsByType(response, wordType):
-	return response[wordType]["syn"]
+	if response == None:
+		return None
+	else:
+		return response[wordType]["syn"]
 	
 def validateWord(wordArrayOfArrays, word, wordType):
 	#first check if word is already in wordArray
@@ -21,13 +30,16 @@ def validateWord(wordArrayOfArrays, word, wordType):
 				return True, arrayNum, wordArrayOfArrays[arrayNum]
 	
 	#else send api request to get synonyms for word and see if a word in wordArray is a synonym
-	wordSynonyms = getSynonymsByType(apiRequest(word), wordType)
-	for word1 in wordSynonyms:
-		for arrayNum in range(len(wordArrayOfArrays)):
-			for word2 in wordArrayOfArrays[arrayNum]:
-				if (word1 == word2):
-					wordArrayOfArrays[arrayNum].append(word)
-					return True, arrayNum, wordArrayOfArrays[arrayNum]
+	wordSynonyms = getSynonymsByType(apiRequest2(word), wordType)
+	if wordSynonyms == None:
+		return False, -1, []
+	else:
+		for word1 in wordSynonyms:
+			for arrayNum in range(len(wordArrayOfArrays)):
+				for word2 in wordArrayOfArrays[arrayNum]:
+					if (word1 == word2):
+						wordArrayOfArrays[arrayNum].append(word)
+						return True, arrayNum, wordArrayOfArrays[arrayNum]
 
 	#if no match found, word is invalid
 	return False, -1, []
