bd5cb96fb0101d66f345f526011719e300c6d121
Joseph Fuerst <fuerstj@oregonstate.edu>
2016-11-30 19:47:12 -0800

    Update player.py

--- Game/player.py
+++ Game/player.py
@@ -1,28 +1,40 @@
 #!/usr/bin/env python
 
-from room import *
+import room
 
 class Player(object):
 
-    inventory = []
+	inventory = []
+	
 
-    def __init__(self):
-        self.curRoom = 0
-        self.health = 10
-        #self.inventory = [None] *10
-        
-    def setName(self, name):
-        self.name = name
-    def getInventory(self):
-        return self.inventory
+	def __init__(self):
+		self.curRoom = 0
+		self.health = 10
+		self.space = 0
+		self.inFight = False
+		self.pTurn = True
+		#self.inventory = [None] *10
+		
+	def setName(self, name):
+		self.name = name
+	def getInventory(self):
+		return self.inventory
 
-    def changeRooms(self, room):
-        self.curRoom = room
-    
-    def addItem(self, item):
-        self.inventory.append(item)
-        item.curRoom = -1
+	def changeRooms(self, room):
+		self.curRoom = room
+	
+	def addItem(self, item):
+		self.inventory.append(item)
+		item.curRoom = -1
+		self.space += 1
 
-    def dropItem(self, item):
-        self.inventory.remove(item)
-        
+	def dropItem(self, item):
+		self.inventory.remove(item)
+		self.space -= 1
+		
+	def invHasSpace(self):
+		if (self.space < 2):
+			return True
+		else:
+			return False
+		
